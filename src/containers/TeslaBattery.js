ead9212e999592a8cb5a8f2fea2fd24d555d5bc1
Shihlin Lu <shihlinlu@gmail.com>
2017-06-28 02:34:30 -0600

    added tesla battery container

---
+++ src/containers/TeslaBattery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TeslaBattery.css';
+
+class TeslaBattery extends React.Component {
+	render() {
+		return (
+			<form className="tesla-battery">
+				<h1>Range Per Charge</h1>
+			</form>
+		)
+	}
+}
+
+export default TeslaBattery;
