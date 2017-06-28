60a1376058b378300cac386e3cd6b791e9e55919
Shihlin Lu <shihlinlu@gmail.com>
2017-06-28 03:09:05 -0600

    updated teslabattery and app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import TeslaBattery from './containers/TeslaBattery';
 
 class App extends Component {
   render() {
