316ddb0383ecef2a9c6b901cf06bd326c5e50cc4
Shihlin Lu <shihlinlu@gmail.com>
2017-06-28 18:09:14 -0600

    Added calculateStats and setState, added additional css to index.css, updated css to app.css

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -3,11 +3,16 @@ import './TeslaBattery.css';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 import TeslaCar from '../components/TeslaCar/TeslaCar';
 import TeslaStats from '../components/TeslaStats/TeslaStats';
+import { getModelData } from '../services/BatteryService';
 
 class TeslaBattery extends React.Component {
 	// constructor to set the initial values
 	constructor(props) {
 		super(props);
+
+		// explicit binding in the constructor is required to access 'this' in the class
+		this.calculateStats = this.calculateStats.bind(this);
+		this.statsUpdate = this.statsUpdate.bind(this);
 
 		this.state = {
 			carstats: [],
@@ -18,6 +23,31 @@ class TeslaBattery extends React.Component {
 				wheels: 19
 			}
 		}
+	}
+
+	calculateStats = (models, value) => {
+		const dataModels = getModelData();
+		return models.map(model => {
+			// ES6 boject destructuring syntax takes out required values and creates references to them
+			const { speed, temperature, climate, wheels } = value;
+			const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+			return {
+				model,
+				miles
+			};
+		});
+	}
+
+	statsUpdate () {
+		const carModels = ['60', '60D', '75', '75D', '90D', 'P100D'];
+		// fetch model info from BatteryService and calculate then update state
+		this.setState({
+			carstats: this.calculateStats(carModels, this.state.config)
+		})
+	}
+
+	componentDidMount() {
+		this.statsUpdate();
 	}
 
 	render() {
