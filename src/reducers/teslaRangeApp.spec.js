d871e3b965617aae03daf1b59ceb0435c389bf33
Shihlin Lu <shihlinlu@gmail.com>
2018-02-14 00:17:51 -0700

    Added actions and reducers to handle air conditioning state changes

---
+++ src/reducers/teslaRangeApp.spec.js
@@ -0,0 +1,52 @@
+import appReducer from './teslaRangeApp';
+
+const initialState = {
+    carstats: [
+        {miles: 246, model: "60"},
+        {miles: 250, model: "60D"},
+        {miles: 297, model: "75"},
+        {miles: 306, model: "75D"},
+        {miles: 336, model: "90D"},
+        {miles: 376, model: "P100D"},
+    ],
+    config: {
+        speed: 55,
+        temperature: 20,
+        climate: true,
+        wheels: 19,
+    }
+};
+
+describe('test reducer', () => {
+    it('should handle initial state', () => {
+        expect(
+            appReducer(undefined, {})
+        ).toEqual(initialState)
+    })
+});
+
+const climateChangeState = {
+    carstats: [
+        {miles:267, model:"60"},
+        {miles:273, model:"60D"},
+        {miles:323, model:"75"},
+        {miles:334, model:"75D"},
+        {miles:366, model:"90D"},
+        {miles:409, model:"P100D"},
+    ],
+    config: {
+        speed: 55,
+        temperature: 20,
+        climate: false,
+        wheels: 19,
+    }
+};
+
+
+it('should handle CHANGE_CLIMATE', () => {
+    expect(
+        appReducer(initialState, {
+            type: 'CHANGE_CLIMATE'
+        })
+    ).toEqual(climateChangeState)
+});
