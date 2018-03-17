ae4c9d7f5c26f7cf9717a32341ef15e680bcdd31
Shihlin Lu <shihlinlu@gmail.com>
2018-03-17 14:48:40 -0600

    Working on styled components

--- src/components/TeslaClimate/TeslaClimate.js
+++ src/components/TeslaClimate/TeslaClimate.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './TeslaClimate.css';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+
+/**
+ * Climate control component
+ */
 const TeslaClimate = (props) => (
-	<div className="tesla-climate">
+    <div className="tesla-climate">
 		<label
 			className={`tesla-climate__item ${props.value ? 'tesla-climate__item--active' : '' }  ${!props.limit ? 'tesla-heat':''}`}
 		>
@@ -19,10 +24,17 @@ const TeslaClimate = (props) => (
 	</div>
 );
 
+// const TeslaClimateStyled = styled(TeslaClimate) `
+//     float: left;
+// `;
+
+
 TeslaClimate.propTypes = {
 	value: PropTypes.bool,
 	limit: PropTypes.bool,
 	handleChangeClimate: PropTypes.func
-}
+};
 
 export default TeslaClimate;
+
+
